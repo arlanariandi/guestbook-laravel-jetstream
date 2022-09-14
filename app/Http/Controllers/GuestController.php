3580c7b76469ac04b3dc77bc3c7691a47346e545
Arlan Ariandi Trislis <arlanariandi@gmail.com>
2022-09-14 20:43:28 +0700

    UPDATE: Update data tables

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Guest;
 use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
 
 class GuestController extends Controller
 {
@@ -13,7 +15,22 @@ class GuestController extends Controller
      */
     public function index()
     {
-        //
+        if (request()->ajax()) {
+            $query = Guest::query();
+
+            return DataTables::of($query)
+                ->addColumn('action', function ($item) {
+                    return '
+                        <a class="inline-block border border-gray-700 bg-gray-700 text-white rounded-md px-2 py-1 m-1 font-semibold transition duration-500 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline"
+                            href="' . route('dashboard.guest.edit', $item->id) . '">
+                            Edit
+                        </a>
+                    ';
+                })
+                ->rawColumns(['action'])
+                ->make();
+        }
+        return view('pages.guest.index');
     }
 
     /**
